b6c60ac222f51e401a6927a11c03cbf2745cf2a4
Brawrdon <BrandonOkeke@gmail.com>
2019-11-09 11:14:51 +0000

    Add GY-30 light intensity sensor

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,16 @@
 #include <Wire.h>
-#include "ArduinoJson.h"
 #include "Adafruit_Sensor.h"
 #include "Adafruit_BME280.h"
 #include "I2Cdev.h"
 #include "MPU6050.h"
+#include "BH1750.h"
 #include "Ticker.h"
 
 Adafruit_BME280 bme;
 MPU6050 accelgyro;
+BH1750 lightMeter;
 
-#define AMOUNT_OF_READINGS 	15
-#define INTERRUPT_GPIO 26
+#define AMOUNT_OF_READINGS 	50
 #define MIN_TEMPERATURE 30.00
 #define MIN_HUMIDITY 40.00
 
@@ -19,12 +19,11 @@ volatile bool detect_movement = true;
 volatile bool allow_reading_sensors = true;
 volatile bool is_reading_sensors = true;
 
-float temparatures[AMOUNT_OF_READINGS], humidities[AMOUNT_OF_READINGS];
 
 void setAllowReadingSensors();
 void setDetectMovement();
 Ticker allow_reading_sensors_timer(setAllowReadingSensors, 30000);
-Ticker read_after_movement_timer(setAllowReadingSensors, 15000);
+Ticker read_after_movement_timer(setAllowReadingSensors, 10000);
 
 float getTemperature(){
 	return bme.readTemperature();
@@ -33,7 +32,11 @@ float getTemperature(){
 float getHumidity(){
 	return bme.readHumidity();
 }
- 
+
+float getLightLevel(){
+	return lightMeter.readLightLevel();
+}
+
 float getAverage(float *data, int size) {
 	float sum = 0.0, average;       
 	
@@ -48,28 +51,35 @@ float getAverage(float *data, int size) {
 
 void readTemperature() {
 	Serial.print("\nReading");
+	float temparatures[AMOUNT_OF_READINGS], humidities[AMOUNT_OF_READINGS], light_levels[AMOUNT_OF_READINGS];
+
 	for (int i = 0; i < AMOUNT_OF_READINGS; i++)
 	{
 		Serial.print(".");
 		temparatures[i] = getTemperature();
 		humidities[i] = getHumidity();
-		delay(500);
+		light_levels[i] = getLightLevel();
+		delay(100);
 	}
 
 	float average_temparature = getAverage(temparatures, AMOUNT_OF_READINGS);
 	float average_humidity = getAverage(humidities, AMOUNT_OF_READINGS);
-	
-	Serial.println("");
-	Serial.print("Average temperature = ");
+	float average_light_level = getAverage(light_levels, AMOUNT_OF_READINGS);
+
+	Serial.print("\nAverage temperature = ");
 	Serial.print(average_temparature);
-	Serial.println(" *C");
+	Serial.print(" *C\t - \t");
 	Serial.println(average_temparature < MIN_TEMPERATURE ? "Temperature is too low!" : "Temperature is good!");
 
 
 	Serial.print("Average humidity = ");
 	Serial.print(average_humidity);
-	Serial.println("%");
+	Serial.print("%\t - \t");
 	Serial.println(average_humidity < MIN_HUMIDITY ? "Humidity is too low!" : "Humidity is good!");
+
+	Serial.print("Average light level = ");
+	Serial.print(average_light_level);
+	Serial.println("lux");
 }
 
 void readSensors() {
@@ -90,18 +100,25 @@ void setAllowReadingSensors() {
 void connectToSensors() {
 	bool is_bme280_connected = false;
 	bool is_gy521_connected = false;
+	bool is_gy30_connected = false;
 
-	while (!is_bme280_connected || !is_gy521_connected)
+
+	while (!is_bme280_connected || !is_gy521_connected || !is_gy30_connected)
 	{
 		delay(500);
 
+		// Setup and Initialise BME280 (Temperature & Humidity)
 		is_bme280_connected = bme.begin();
-		// Setup BME280 (Temperature & Humidity)
 		Serial.println(is_bme280_connected ? "\nBME280 connection successful." : "\nBME280 connection failed.");
 
-		//Setup GY-521 / MPU6050 (Gyroscope)
+		//Setup and Initialise GY-521 / MPU6050 (Gyroscope)
 		is_gy521_connected = accelgyro.testConnection();
 		Serial.println(is_gy521_connected ? "GY-521 connection successful." : "GY-521 connection failed.");
+
+		//Setup and Initialise GY-30 / BH1750 (Light Sensor)
+		is_gy30_connected = lightMeter.begin();
+		Serial.println(is_gy30_connected ? "GY-30 connection successful." : "GY-30 connection failed.");
+
 	}
 
 	// Initialise GY-521 / MPU6050 (Gyroscope)
