84492a4c22ff690ad5e0f60a10922ef835190ed6
Brawrdon <BrandonOkeke@gmail.com>
2019-11-09 11:51:52 +0000

    Replace gyroscope with vibration sensor
    The GY-521 (MPU6050) wasn't useful for detecting when the device was placed down and it was difficult to work with. A KY-002 vibration sesnor has been used instead. It also means there's no longer a need for a 10 second timer after movement was detected as we now know when the device was placed, not when it was picked up; the sensors are only meant to start reading when "movement stopped".

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,25 @@
 #include <Wire.h>
 #include "Adafruit_Sensor.h"
 #include "Adafruit_BME280.h"
-#include "I2Cdev.h"
-#include "MPU6050.h"
 #include "BH1750.h"
 #include "Ticker.h"
 
 Adafruit_BME280 bme;
-MPU6050 accelgyro;
 BH1750 lightMeter;
 
+#define VIBRATION_PIN 15
 #define AMOUNT_OF_READINGS 	50
 #define MIN_TEMPERATURE 30.00
 #define MIN_HUMIDITY 40.00
 
-bool has_moved;
+volatile bool has_moved = false;
 volatile bool detect_movement = true;
 volatile bool allow_reading_sensors = true;
 volatile bool is_reading_sensors = true;
 
 
 void setAllowReadingSensors();
-void setDetectMovement();
 Ticker allow_reading_sensors_timer(setAllowReadingSensors, 30000);
-Ticker read_after_movement_timer(setAllowReadingSensors, 10000);
 
 float getTemperature(){
 	return bme.readTemperature();
@@ -88,7 +84,6 @@ void readSensors() {
 	is_reading_sensors = false;
 	allow_reading_sensors = false;
 	detect_movement = true;
-	read_after_movement_timer.stop();
 	allow_reading_sensors_timer.start();
 	
 }
@@ -99,21 +94,19 @@ void setAllowReadingSensors() {
 
 void connectToSensors() {
 	bool is_bme280_connected = false;
-	bool is_gy521_connected = false;
 	bool is_gy30_connected = false;
 
 
-	while (!is_bme280_connected || !is_gy521_connected || !is_gy30_connected)
+	while (!is_bme280_connected || !is_gy30_connected)
 	{
 		delay(500);
 
-		// Setup and Initialise BME280 (Temperature & Humidity)
+		// Setup and Initialise BME280 (Temperature & Humidity Sensor)
 		is_bme280_connected = bme.begin();
 		Serial.println(is_bme280_connected ? "\nBME280 connection successful." : "\nBME280 connection failed.");
 
-		//Setup and Initialise GY-521 / MPU6050 (Gyroscope)
-		is_gy521_connected = accelgyro.testConnection();
-		Serial.println(is_gy521_connected ? "GY-521 connection successful." : "GY-521 connection failed.");
+		//Setup and Initialise KY-002  (Vibration Sensor)
+		pinMode(VIBRATION_PIN, INPUT);
 
 		//Setup and Initialise GY-30 / BH1750 (Light Sensor)
 		is_gy30_connected = lightMeter.begin();
@@ -121,10 +114,6 @@ void connectToSensors() {
 
 	}
 
-	// Initialise GY-521 / MPU6050 (Gyroscope)
-	accelgyro.initialize();
-	accelgyro.setMotionDetectionThreshold(2);
-	accelgyro.setMotionDetectionDuration(5);
 }
 
 void setup() {
@@ -139,18 +128,21 @@ void setup() {
 
 void loop() {
 	allow_reading_sensors_timer.update();
-	read_after_movement_timer.update();
-	if(detect_movement && !is_reading_sensors) {
 
-		has_moved = accelgyro.getXPosMotionDetected() || accelgyro.getXNegMotionDetected() || accelgyro.getYPosMotionDetected() 
-				|| accelgyro.getYNegMotionDetected() || accelgyro.getZPosMotionDetected() || accelgyro.getZNegMotionDetected();
+	if (digitalRead(VIBRATION_PIN) == LOW)
+	{
+		has_moved = true;
+	} else {
+		has_moved = false;
+	}
+	
+	if((detect_movement && !is_reading_sensors) && has_moved) {
 
-		if(has_moved) {
 			Serial.println("\nForcing read from sensors as movement has been detected.");
 			allow_reading_sensors_timer.stop();
-			read_after_movement_timer.start();
+			allow_reading_sensors = true;
 			detect_movement = false;
-		}
+		
 	}
 
 	if(allow_reading_sensors) {
