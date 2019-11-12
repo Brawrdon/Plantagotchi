9e7715d25a9f98fd27f1b632cd9ae03e39d95a08
Brawrdon <BrandonOkeke@gmail.com>
2019-11-12 00:16:37 +0000

    Use namespace instead of class

--- include/BLEWrapper.h
+++ include/BLEWrapper.h
@@ -1,7 +1,6 @@
 #include <string>
 
-class BLEWrapper {
-    public:
-        static void setup(std::string* ssid, std::string* password);
-        static void setWifiStatus(std::string wifi_status);
+namespace BLEWrapper {
+    void setup(std::string* ssid, std::string* password);
+    void setWifiStatus(std::string wifi_status);
 };
--- src/BLEWrapper.cpp
+++ src/BLEWrapper.cpp
@@ -22,16 +22,16 @@ class PasswordCharacteristicCallback: public BLECharacteristicCallbacks {
 };
 
 void BLEWrapper::setup(string* ssid_pointer, string* password_pointer) {
-    ::ssid_pointer = ssid_pointer;
-    ::password_pointer = password_pointer;
-
-    BLEUUID wifi_connection_service_uuid("66037b25-aa3b-41ee-a533-9b0da129f4d0");
-
-    BLEUUID ssid_characteristic_uuid("9a1d34d4-e3ab-4978-bc0c-7a7ca0ab9ae7");
-    BLEUUID password_characteristic_uuid("c56e90af-3b2d-4fe2-ad0b-dcc98207102a");
+	::ssid_pointer = ssid_pointer;
+	::password_pointer = password_pointer;
+	
+	BLEUUID wifi_connection_service_uuid("66037b25-aa3b-41ee-a533-9b0da129f4d0");
+	
+	BLEUUID ssid_characteristic_uuid("9a1d34d4-e3ab-4978-bc0c-7a7ca0ab9ae7");
+	BLEUUID password_characteristic_uuid("c56e90af-3b2d-4fe2-ad0b-dcc98207102a");
 	BLEUUID wifi_connected_characteristic_uuid("2ac31e6b-04b8-4a1a-850e-f0e574ca7fc5");
 
-    // Create BLE Server
+	// Create BLE Server
 	BLEDevice::init("Plantagotchi");
 	BLEServer* server_pointer = BLEDevice::createServer();
 	  
@@ -44,17 +44,17 @@ void BLEWrapper::setup(string* ssid_pointer, string* password_pointer) {
 	wifi_connected_characteristic = wifi_connection_service->createCharacteristic(wifi_connected_characteristic_uuid, BLECharacteristic::PROPERTY_READ);
 
 	wifi_connected_characteristic->setValue("DISCONNECTED");
-
-    ssid_characteristic->setCallbacks(new SSIDCharacteristicCallback());
-    password_characteristic->setCallbacks(new PasswordCharacteristicCallback());
-
+	
+	ssid_characteristic->setCallbacks(new SSIDCharacteristicCallback());
+	password_characteristic->setCallbacks(new PasswordCharacteristicCallback());
+	
 	wifi_connection_service->start();
-
+	
 	// Advertise BLE Server
 	BLEAdvertising* advertising_pointer = BLEDevice::getAdvertising();
 	advertising_pointer->addServiceUUID(wifi_connection_service_uuid);
 	advertising_pointer->setScanResponse(true);
-
+	
 	// Functions to help with iPhone connectivity issues
 	advertising_pointer->setMinPreferred(0x06);  
  	advertising_pointer->setMinPreferred(0x12);
