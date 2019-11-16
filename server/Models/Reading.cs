8b426db08a2ca132a3409e6d2949b6f6d1f6ab5c
Brawrdon <BrandonOkeke@gmail.com>
2019-11-16 09:43:26 +0000

    Initial commit of ASP.NET Core server

---
+++ server/Models/Reading.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Plantagotchi.Models
+{
+    public class Reading
+    {
+        public int Temperature { get; set; }
+      
+        public int Humidity { get; set; }
+        
+        public int LightLevel { get; set; }
+
+    }
+}
